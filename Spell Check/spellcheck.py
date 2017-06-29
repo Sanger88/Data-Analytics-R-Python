537de8ea91ef2591667b45e4b6f94a8b384253c1
Sanger88 <sanel.gerzic@hotmail.com>
2017-06-29 13:01:43 +0100

    handle multiple sentences.

--- Spell Check/spellcheck.py
+++ Spell Check/spellcheck.py
@@ -2,11 +2,17 @@ class SpellChecker:
 	def __init__(self):
 		self.words = []
 	def load_words(self, file_name):
-		words = open(file_name).readlines()
-		words = map(lambda x: x.strip(), self.words)
+		self.words = open(file_name).readlines()
+		self.words = map(lambda x: x.strip().lower(), self.words)
 #		return words
+
+	def load_file(self, file_name):
+		lines = open(file_name).readlines()
+		lines = map(lambda x: x.strip(), lines)
+		return lines
+		
 	def check_word(self, word):
-		return word.strip(' ').lower() in self.words
+		return word.strip('.').lower() in self.words
 		
 	def check_words(self, sentence):
 		words_to_check = sentence.split(' ')
@@ -17,8 +23,17 @@ class SpellChecker:
 				failed_words.append(word)
 		return failed_words
 
+	def check_document(self, file_name):
+		self.sentences = self.load_file(file_name)
+		failed_words_in_sentences = []
+		index = 0
+		for sentence in self.sentences:
+			failed_words_in_sentences.extend(self.check_words(sentence))
+			index = index + 1
+		return failed_words_in_sentences
+
 if __name__ == '__main__':
-    spellChecker = SpellChecker()
-    spellChecker.load_words('spell.words')
-    print(spellChecker.check_word('zygotic'))
-    print(spellChecker.check_words('zygotic mistasdas elementary'))
+	spellChecker = SpellChecker()
+	spellChecker.load_words('spell.words')
+	print(spellChecker.check_word('zygotic'))
+	print(spellChecker.check_words('zygotic mistasdas elementary'))
--- Spell Check/spellcheck_test.py
+++ Spell Check/spellcheck_test.py
@@ -4,31 +4,32 @@ from spellcheck import SpellChecker
 
 class TestSpellChecker(unittest.TestCase):
 
-    def setUp(self):
-        self.spellChecker = SpellChecker()
-        self.spellChecker.load_words('spell.words')
-
-    def test_spell_checker(self):
+	def setUp(self):
+		self.spellChecker = SpellChecker()
+		self.spellChecker.load_words('spell.words')
+	
+	def test_spell_checker(self):
 		self.assertTrue(self.spellChecker.check_word('zygotic'))
-        failed_words = self.spellChecker.check_words(
+		failed_words = self.spellChecker.check_words(
 				'zygotic mistasdas elementary')
 		self.assertEquals(1, len(failed_words))
-        self.assertEquals('mistasdas', failed_words[0])
-        self.assertEquals(0, len(self.spellChecker.check_words(
+		self.assertEquals('mistasdas', failed_words[0])
+		self.assertEquals(0, len(self.spellChecker.check_words(
 				'our first correct sentence')))
-        # handle case sensitivity
+		# handle case sensitivity
 		self.assertEquals(0, len(self.spellChecker.check_words(
 				'Our first correct sentence')))
-        # handle full stop
-        self.assertEquals(0, len(self.spellChecker.check_words(
+		# handle full stop
+		self.assertEquals(0, len(self.spellChecker.check_words(
 				'Our first correct sentence.')))
-        failed_words = self.spellChecker.check_words(
+		failed_words = self.spellChecker.check_words(
 				'zygotic mistasdas spelllleeeing elementary')
-        self.assertEquals(2, len(failed_words))
-        self.assertEquals('mistasdas', failed_words[0])
-        self.assertEquals('spelllleeeing', failed_words[1])
+		self.assertEquals(2, len(failed_words))
+		self.assertEquals('mistasdas', failed_words[0])
+		self.assertEquals('spelllleeeing', failed_words[1])
+		self.assertEqual(21, len(self.spellChecker.check_document('spell.words')))
+		
 
-	
 	
 	
 	
@@ -40,4 +41,4 @@ class TestSpellChecker(unittest.TestCase):
 		#self.assertTrue(self.spellChecker.check_words('Our first correct sentence'))
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
